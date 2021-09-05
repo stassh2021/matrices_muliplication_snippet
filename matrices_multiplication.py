e0b287328dd7f141c153737bb4c5453fd8b67750
StasSh <stassh@6degrees.tech>
2021-09-05 12:51:30 +0300

    gitignore02

---
+++ matrices_multiplication.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Matrix M:
+x = np.arange(9)
+M = x.reshape(3, 3)
+
+# vector v:
+v = np.array([[1, 2, 3]]).reshape(3,1)
+
+# print them:
+print(f'M = \n {M}')
+print(f'v = \n {v}')
+
+# print the product:
+M_dot_v = np.dot(M, v)
+print(f'<M, v> = \n {M_dot_v}')
